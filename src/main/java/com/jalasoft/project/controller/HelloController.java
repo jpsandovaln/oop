131cb5aaedbd804469763dda1167e67d153a7593
JDELGADILP-DV11\jp <juan.pablo.delgadillo.peredo@gmail.com>
2020-07-10 12:44:10 -0400

    Adding my solution for task 2, point 3 with the correct file

--- src/main/java/com/jalasoft/project/controller/HelloController.java
+++ src/main/java/com/jalasoft/project/controller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author HP
@@ -15,7 +19,12 @@ public class HelloController {
 
     @PostMapping
     public String sayHello(@RequestParam(value = "name") String name,
-                           @RequestParam(value = "lastName") String lastName) {
-        return "Hello " + name + " " + lastName;
+                           @RequestParam(value = "lastName") String lastName,
+                           @RequestParam(value = "file")MultipartFile file) throws IOException {
+
+                File dest = new File("c://uploads//testFile1.txt");
+                file.transferTo(dest);
+
+        return "Hello " + name + " " + lastName + " your file " + file.getOriginalFilename() + " was saved at C:/uploads folder" ;
     }
 }
