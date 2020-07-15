82ee22c6c17d5e4726a13590b757d030aac72e40
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2020-07-15 19:16:44 -0400

    Added request paramater and validations

--- src/main/java/com/jalasoft/project/controller/endpoint/ExecuteController.java
+++ src/main/java/com/jalasoft/project/controller/endpoint/ExecuteController.java
@@ -1,11 +1,11 @@
 package com.jalasoft.project.controller.endpoint;
 
+import com.jalasoft.project.controller.request.RequestParam;
 import com.jalasoft.project.model.ExecuteJava;
 import com.jalasoft.project.model.parameter.JavaParameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -25,34 +25,19 @@ import java.nio.file.StandardCopyOption;
 public class ExecuteController {
 
     @PostMapping("/execute")
-    public String execute(@RequestParam(value = "lang") String lang,
-                          @RequestParam(value = "version") String version,
-                          @RequestParam(value = "file") MultipartFile file) {
-
-        if (lang == null || lang.isEmpty()) {
-            return "Invalid lang.";
-        }
-        if (!"java".equals(lang)) {
-            return "Invalid lang.";
-        }
-        if (version.isEmpty()) {
-            return "Invalid version";
-        }
-        if (!"1.8".equals(version)) {
-            return "Invalid version";
-        }
-        if (file == null || file.isEmpty()) {
-            return "Invalid File";
-        }
+    public String execute(RequestParam param) {
         try {
+            param.validate();
             Files.createDirectories(Paths.get("javaProject/"));
-            Path path = Paths.get("javaProject/" + file.getOriginalFilename());
-            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+            Path path = Paths.get("javaProject/" + param.getFile().getOriginalFilename());
+            Files.copy(param.getFile().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
             File javaFile = new File(path.toString());
             String javaPath = "D:\\POO\\oop\\thirdParty\\java\\win\\jdk1.8.0_251\\bin\\";
             ExecuteJava executeJava = new ExecuteJava();
             return executeJava.execute(new JavaParameter(javaPath, javaFile));
         } catch (IOException ex) {
+            return ex.getMessage();
+        } catch (Exception ex) {
             return ex.getMessage();
         }
     }
