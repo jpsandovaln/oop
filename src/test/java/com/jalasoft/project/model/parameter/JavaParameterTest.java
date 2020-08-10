bb6cb44ba3bef749f07e109eb54ae82c49137364
Paolo Sandoval <paolo.sandoval@jalasoft.com>
2020-08-09 23:33:13 -0400

    Added unit test

---
+++ src/test/java/com/jalasoft/project/model/parameter/JavaParameterTest.java
@@ -0,0 +1,12 @@
+package com.jalasoft.project.model.parameter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JavaParameterTest {
+
+    @Test
+    public void validate() {
+    }
+}
